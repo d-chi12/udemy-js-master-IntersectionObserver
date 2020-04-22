4ccc860d2aab87814d88f3a26c3a735c61530b7d
USER <user@USERnoMacBook-ea.local>
2020-04-22 16:46:21 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const child = document.querySelector('.child');
+const cb = function(entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            console.log('invew');
+            entry.target.classList.add('inview')
+            // observe.unobserve(entry.target);            
+        } else {
+            console.log('out view');
+        }
+    });
+    // alert('intersecting');
+}
+
+// const options ={
+//     root: null,
+//     rootMargin: "300px 0px",
+//     threshold: [0, 0.5, 1]
+// };
+
+const io = new IntersectionObserver(cb);
+io.observe(child);
